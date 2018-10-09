02de1e283d6f63e8bc83ecd90c13c4ce60fbac26
EleryanYuri <saunier.aurelien.mail@gmail.com>
2018-10-09 11:23:05 +0200

    display pizza

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Pizza} from "../app/pizza"
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Pizza LP4A';
+  pizzaParent: Array<Pizza> = [
+    {name: "marguerite", price: 12, url: "https://media.istockphoto.com/photos/pizza-on-white-background-picture-id635675852"},
+    {name: "reine", price: 10, url: "https://thumbs.dreamstime.com/z/pizza-16727090.jpg"},
+]
 }
