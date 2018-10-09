ef9dcae5c86191a28ac8758a6834ea7d6ac65466
EleryanYuri <saunier.aurelien.mail@gmail.com>
2018-10-09 11:33:36 +0200

    display pizza

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,5 @@ import {Pizza} from "../app/pizza"
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  pizzaParent: Array<Pizza> = [
-    {name: "marguerite", price: 12, url: "https://media.istockphoto.com/photos/pizza-on-white-background-picture-id635675852"},
-    {name: "reine", price: 10, url: "https://thumbs.dreamstime.com/z/pizza-16727090.jpg"},
-]
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PizzaComponent } from './pizza/pizza.component';
+import { PizzaListComponent } from './pizza-list/pizza-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaComponent
+    PizzaComponent,
+    PizzaListComponent
   ],
   imports: [
     BrowserModule
